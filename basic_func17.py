d5cfef51e122095ec20ddc1fb5de5c3aada2a2f9
tajriba <quvvatullayev@gmail.com>
2022-06-18 22:57:20 +0300

    basic_func17.py

--- basic_func17.py
+++ basic_func17.py
@@ -1,3 +1,12 @@
 # Create a function called main.
 # Create function arguments a.
 # Assign the value pi to the variable "a" and return.
+
+
+import math
+
+
+def main(a):
+    return a
+
+print(main(math.pi))
