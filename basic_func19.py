5bb083b7c95efc01e8b63a14d5b9457c05305649
tajriba <quvvatullayev@gmail.com>
2022-06-18 23:04:57 +0300

    basic_func19.py

--- basic_func19.py
+++ basic_func19.py
@@ -1,3 +1,9 @@
 # Create a function called main.
 # Create function arguments a and b.
 # Return the absolute value of the difference between a and b.
+
+
+def main(a, b):
+    return a, b
+
+print(main(4,9))
